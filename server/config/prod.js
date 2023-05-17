447c880811a1f4be714bfd72b89aa63ce6dcfcd7
Shahid <shahid@ensarsolutions.com>
2023-05-17 21:50:31 +0530

    first commit

---
+++ server/config/prod.js
@@ -0,0 +1,9 @@
+module.exports = {
+  DB_URI: 'mongodb://localhost:27017/hotelreact',
+  SECRET: 'abc',
+  AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+  AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+  STRIPE_SK: process.env.STRIPE_SK
+}
+
+
